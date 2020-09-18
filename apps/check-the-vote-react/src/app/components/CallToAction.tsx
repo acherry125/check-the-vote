f8062a252c66a48561fe12b1cb4d89dd745ad8b9
Kenneth Lu Diaz <ludiazken@gmail.com>
2020-09-18 13:21:53 -0400

    Added roboto condesed font to CTA info. Styled the CTA button.

--- apps/check-the-vote-react/src/app/components/CallToAction.tsx
+++ apps/check-the-vote-react/src/app/components/CallToAction.tsx
@@ -1,5 +1,4 @@
 import React, {useState} from "react";
-import NavigationLink from './NavitgationLink';
 
 
 export const CallToAction = () => {
@@ -7,8 +6,8 @@ export const CallToAction = () => {
   return (
    <div className="call-to-action">
      <h2 className="call-to-action-title">Why Check The Vote?</h2>
-     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, modi itaque ut aliquid similique voluptatibus consequatur ipsa, libero deserunt placeat odio assumenda non at tempora fugit quae veniam illo excepturi!</p>
-     <a href="#">Learn More</a>
+     <p className="call-to-action-info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, modi itaque ut aliquid similique voluptatibus consequatur ipsa, libero deserunt placeat odio assumenda non at tempora fugit quae veniam illo excepturi!</p>
+     <a className="call-to-action-btn" href="#">Learn More</a>
    </div>
   )
 }
